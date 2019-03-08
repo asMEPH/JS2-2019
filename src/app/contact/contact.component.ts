a571d3783a3239f0d7757355afc60ae13d05c71c
g <g>
2019-03-08 19:16:38 +0100

    gg

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.less']
 })
 export class ContactComponent implements OnInit {
-
+  adres = "Avenida de la Victioria 1, Lima, Peru";
+  houseNumber = 2;
+  private x = 0;
+  userzy = ['Wu Xiaoli','Nash Gordon','Aleksii Burow'];
   constructor() { }
 
   ngOnInit() {
+    console.log('klasa kontaktowa zostala zbudowana')
+  }
+
+  mojaAkcja() {
+    this.x = this.x + 1;
+    console.log('akcja wykonana! ' + this.x + ' razy');
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-
+  name: string;
   constructor() { }
 
   ngOnInit() {
